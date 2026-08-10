2d050e5b257f5e609ccc8b33ea666cccebc8e5f8
mohammadrad <mohammadafzalirad2001@gmail.com>
2026-08-10 14:26:46 +0330

    Fix: Fix testing security problem with env file

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv(Path(__file__).resolve().parent.parent / '.env.test', override=True)
+load_dotenv()
 
 from sqlalchemy import create_engine, text
 from app.db_base import Base
